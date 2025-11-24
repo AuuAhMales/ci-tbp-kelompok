0a49d11a4fc7ac700face6a02f75b164c1852b74
AuuAhMales <auautayo4671uaua@gmail.com>
2025-11-24 21:00:49 +0800

    fix: add node and jest globals to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,15 @@
 const js = require("@eslint/js");
 
 module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        test: "readonly",
+        expect: "readonly"
+      }
+    }
+  },
   js.configs.recommended
 ];
